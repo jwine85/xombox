9e981d2b0fdc1c4195150ce96ba86d843bf878d1
Justin <jwine85@xombox.net>
2013-11-19 00:46:39 -0500

    updating js

--- js/xombox.js
+++ js/xombox.js
@@ -3,9 +3,11 @@ $(document).ready(function(){
 	document.createElement("article");
 	document.createElement("section");
 	
+	var $window = $(window);
+
 	$('section[data-type="background"]').each(function(){
 		var $bgobj = $(this) //assigning the object
-		$(window).scroll(function(){
+		$window.scroll(function(){
 			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
 			//Get Background Position
 			var coords = '50% ' + yPos + 'px'; 
