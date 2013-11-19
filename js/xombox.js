8a5ef5bad0d2ff20550757d8458df0eab1516766
Justin <jwine85@xombox.net>
2013-11-19 00:42:15 -0500

    Debugging js

--- js/xombox.js
+++ js/xombox.js
@@ -1,17 +1,17 @@
-// $(document).ready(function(){
-// 	// Create HTML5 elements for IE
-// 	document.createElement("article");
-// 	document.createElement("section");
+$(document).ready(function(){
+	// Create HTML5 elements for IE
+	document.createElement("article");
+	document.createElement("section");
 	
-// 	$('section[data-type="background"]').each(function(){
-// 		var $bgobj = $(this) //assigning the object
-// 		$(window).scroll(function(){
-// 			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-// 			//Get Background Position
-// 			var coords = '50% ' + yPos + 'px'; 
-// 			//Move Background
-// 			$bgobj.css({backgroundPosition: coords});
-// 			console.log(coords);
-// 		});
-// 	});
-// });
+	$('section[data-type="background"]').each(function(){
+		var $bgobj = $(this) //assigning the object
+		$(window).scroll(function(){
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+			//Get Background Position
+			var coords = '50% ' + yPos + 'px'; 
+			//Move Background
+			$bgobj.css({backgroundPosition: coords});
+			console.log(coords);
+		});
+	});
+});
