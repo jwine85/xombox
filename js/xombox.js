50b1af73a73f251524a80797e8abe882beaa907e
Justin <jwine85@xombox.net>
2013-11-19 00:50:47 -0500

    Debugging js

--- js/xombox.js
+++ js/xombox.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	// Create HTML5 elements for IE
 	document.createElement("article");
 	document.createElement("section");
-	
+
 	var $window = $(window);
 
 	$('section[data-type="background"]').each(function(){
@@ -13,6 +13,7 @@ $(document).ready(function(){
 			var coords = '50% ' + yPos + 'px'; 
 			//Move Background
 			$bgobj.css({backgroundPosition: coords});
+			console.log("window scroll top: " + $window.scrollTop());
 			console.log(coords);
 		});
 	});
